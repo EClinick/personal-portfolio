20e57bbaf76f14879dcf3d477987ebf0fa594d68
EClinick <Ethan@Clinick.net>
2024-11-13 20:10:16 -0800

    Update .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-// /vite.config.ts
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Personal-Portfolio/', // Ensure this matches your repo name
+  base: '/Personal-Portfolio/', // Ensure this matches your repository name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
