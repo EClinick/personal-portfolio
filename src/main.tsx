ab4e60ac4b5e7f3d93d1f5449ab878cd3c8b7d89
EClinick <Ethan@Clinick.net>
2024-11-13 19:55:17 -0800

    Removed dist from gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-// main.tsx
+// /src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // Change to HashRouter
+import { HashRouter } from 'react-router-dom'; // Import HashRouter
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter basename="/Personal-Portfolio">
+    <HashRouter basename="/Personal-Portfolio"> {/* Use HashRouter */}
       <App />
     </HashRouter>
   </StrictMode>
